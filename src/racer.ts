26f7c2e9bacfbe29b027d7120039c885d4354a2f
goodmirow-1 <57578975+goodmirow-1@users.noreply.github.com>
2024-02-15 14:10:46 +0900

    {blue} racer measure func -> ping

--- src/racer.ts
+++ src/racer.ts
@@ -1,16 +1,12 @@
 export async function Racer(a: string, b: string): Promise<string> {
-       const aDuration = await measureResponseTime(a);
-       const bDuration = await measureResponseTime(b);
-
-       if (aDuration < bDuration) {
-           return a;
-       }
-
-       return b;
+           const winner = await Promise.race([ping(a), ping(b)]);
+           return winner;
 }
 
-async function measureResponseTime(url: string) : Promise<number> {
-    const start = Date.now();
-    await fetch(url);
-    return Date.now() - start;
+function ping(url: string): Promise<string> {
+   return new Promise(resolve => {
+       fetch(url)
+           .then(() => resolve(url))
+           .catch(() => resolve('')); // or you can resolve with some error value
+   });
 }
