7e774d7afa1b755135d9ad38ae5001125c68a439
gukhyeon <goodmirow@gmail.com>
2024-02-21 13:32:51 +0900

    (green) v1 reflection walk func refactoring

--- test/reflection.test.ts
+++ test/reflection.test.ts
@@ -15,3 +15,29 @@ test('test reflection', () => {
 
     expect(got[0]).toBe(expected);
 })
+
+interface struct {
+    Name: string;
+    Input: { [key: string]: any};
+    ExpectedCalls: string[];
+}
+
+const cases: struct[] = [
+    {
+        Name: "Struct with one string field",
+        Input: { Name: "Chris" },
+        ExpectedCalls: ["Chris"],
+    },
+];
+
+describe('test walk', () => {
+    cases.forEach( struct => {
+        test(`Test case: ${struct.Name}`, () => {
+            let got: string[] = [];
+            walk(struct.Input, (input: string) => {
+                got.push(input);
+            });
+            expect(got).toEqual(struct.ExpectedCalls);
+        });
+    });
+})
