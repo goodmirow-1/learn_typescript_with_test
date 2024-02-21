f01538036960f5f25af79c32a813829b7d197976
gukhyeon <goodmirow@gmail.com>
2024-02-21 22:48:55 +0900

    (green) reflection walk func cases refactoring with slice

--- src/reflection.ts
+++ src/reflection.ts
@@ -1,21 +1,28 @@
 
 export function walk(x: { [key: string]: any }, callback: (input: string) => void) {
-    if (Array.isArray(x)) {
-        x.forEach((item: any) => {
-            walk(item, callback);
-        });
-        return;
+    const val = getValue(x);
+
+    switch (typeof val) {
+        case 'object':
+            if (Array.isArray(val)){ val.forEach((item: any) => walk(item, callback)); break;}
+
+            for (const key in val) {
+                if (val.hasOwnProperty(key)) {
+                    walk(val[key], callback);
+                }
+            }
+
+            break;
+        case 'string':
+            callback(val);
+            break;
     }
+}
 
-    const keys = Object.keys(x);
-
-    for(let i = 0 ; i < keys.length ; ++i){
-        const value = x[keys[i]];
-
-        switch(typeof value){
-            case 'string': callback(value); break;
-            case 'object': walk(value, callback); break;
-            default: break;
-        }
+function getValue(x: { [key: string]: any}) : any {
+    if (typeof x === 'object' && x !== null && !Array.isArray(x)) {
+        return Object.values(x);
+    } else {
+        return x;
     }
 }
